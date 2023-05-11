d61e8e8b70d13e474f41608c6f41d486060464b3
BONCHEUL KOO <goobncl@gmail.com>
2023-05-11 10:21:47 +0400

    fps 계산 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QTimer
 from PySide6.QtGui import QImage, QPixmap
@@ -11,10 +12,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loader = QUiLoader()
-        self.ui = loader.load('COMPUTER_VISION.ui', None)
+        self.ui = loader.load('./Form Files/COMPUTER_VISION.ui', None)
         self.ui.show()
 
+        self.frames = 0
+        self.last_time = time.time()
+        self.fps = 0.0
+        self.statusbar = self.ui.statusBar()
+        self.timer_fps = QTimer()
+        self.timer_fps.timeout.connect(self.update_fps)
+        self.timer_fps.start(10)
+
         self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+
         self.clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8, 6))
         self.apply_clahe = False
 
@@ -36,10 +49,24 @@ class MainWindow(QMainWindow):
         else:
             self.ui.claheBtn.setFont(self.font_normal)
 
+    def calculate_fps(self):
+        now = time.time()
+        duration = now - self.last_time
+        if duration >= 1.0:
+            self.fps = self.frames / duration
+            self.frames = 0
+            self.last_time = now
+
+    def update_fps(self):
+        self.statusbar.showMessage(f"FPS: {self.fps:.8f}")
+
     def update_frame(self):
         ret, frame = self.cap.read()
         if not ret:
             return
+
+        self.frames += 1
+        self.calculate_fps()
 
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if self.apply_clahe:
@@ -52,7 +79,9 @@ class MainWindow(QMainWindow):
         self.ui.videoLabel.setPixmap(pixmap)
 
     def closeEvent(self, event):
+        self.timer.stop()
         self.cap.release()
+        event.accept()
 
 
 if __name__ == '__main__':
